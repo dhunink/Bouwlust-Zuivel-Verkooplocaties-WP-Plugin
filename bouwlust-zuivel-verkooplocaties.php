17e3f9f27c21ec765565d67d587045afd78c06f9
Dennis Hunink <dennishunink@me.com>
2026-08-21 15:52:27 +0200

    Bump to 0.1.6 and repair ACF field aliases

--- bouwlust-zuivel-verkooplocaties.php
+++ bouwlust-zuivel-verkooplocaties.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Bouwlust Zuivel Verkooplocaties
  * Description: Beheer en periodieke import van zuivel-verkooplocaties naar het ACF-berichttype zuivel_verkooppunt, inclusief Google-geocoding.
- * Version: 0.1.5
+ * Version: 0.1.6
  * Author: Hoeve Bouwlust
  */
 
@@ -10,12 +10,14 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('BZV_VERSION', '0.1.5');
+define('BZV_VERSION', '0.1.6');
 define('BZV_PLUGIN_FILE', __FILE__);
 define('BZV_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 require_once BZV_PLUGIN_DIR . 'includes/class-bzv-file-reader.php';
 require_once BZV_PLUGIN_DIR . 'includes/class-bzv-importer.php';
+require_once BZV_PLUGIN_DIR . 'includes/class-bzv-acf-bridge.php';
 require_once BZV_PLUGIN_DIR . 'includes/class-bzv-admin.php';
 
+BZV_ACF_Bridge::init();
 BZV_Admin::init();
