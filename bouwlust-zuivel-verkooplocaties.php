153985e126479a356ce3e07e47801c00319ef2a2
Dennis Hunink <dennishunink@me.com>
2026-08-21 12:36:44 +0200

    Voeg plugin bootstrap voor v0.1.1 toe

---
+++ bouwlust-zuivel-verkooplocaties.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Bouwlust Zuivel Verkooplocaties
+ * Description: Beheer en periodieke import van zuivel-verkooplocaties naar het ACF-berichttype zuivel_verkooppunt, inclusief Google-geocoding.
+ * Version: 0.1.1
+ * Author: Hoeve Bouwlust
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('BZV_VERSION', '0.1.1');
+define('BZV_PLUGIN_FILE', __FILE__);
+define('BZV_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+require_once BZV_PLUGIN_DIR . 'includes/class-bzv-file-reader.php';
+require_once BZV_PLUGIN_DIR . 'includes/class-bzv-importer.php';
+require_once BZV_PLUGIN_DIR . 'includes/class-bzv-admin.php';
+
+BZV_Admin::init();
